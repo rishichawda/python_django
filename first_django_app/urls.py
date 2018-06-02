2fe38eb22e7069486f703f8048cee1a5ad7f5e6e
Rishi Kumar <chawda.rishii@gmail.com>
2018-06-02 21:43:44 +0530

    add catalog and redirects

--- first_django_app/urls.py
+++ first_django_app/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-    path('', include('polls.urls')),
-]
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
