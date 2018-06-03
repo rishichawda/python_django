992ca1fa06722936a36741feb3a4654ca4628d68
Rishi Kumar <chawda.rishii@gmail.com>
2018-06-03 13:28:55 +0530

    add inline editing view

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,23 +2,38 @@ from django.contrib import admin
 from .models import Book, Genre, BookInstance, Author, Language
 
 # Register your models here.
-# admin.site.register(Book)
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'language')
-
-admin.site.register(Genre)
 
 # admin.site.register(BookInstance)
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_date')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_date')
+        }),
+    )
 
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+# admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'language', 'display_genre')
+
+    inlines = [BookInstanceInline]
+
+admin.site.register(Genre)
 
 # admin.site.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    
 admin.site.register(Author, AuthorAdmin)
 
 admin.site.register(Language)
